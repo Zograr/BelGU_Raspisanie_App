dd36cfd81001af08b1ea732a7714291625ea73b9
Zograr <shipanton2603@gmail.com>
2026-05-17 17:18:11 +0300

    v1.19.16

---
+++ v1.19.16/app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "ru.zograr.belguschedule"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ru.zograr.belguschedule"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 84
+        versionName = "1.19.16"
+    }
+}
